70eb22a816cd8ce5507a62ee466fca235da65de1
Marcin Kiklica <kiklica.m@gmail.com>
2019-06-10 14:33:07 +0200

    Second test with google.truth.

--- qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
@@ -2,25 +2,41 @@ package com.jsystems.qa.qajunit;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class JunitTest {
 
+    final String stringTestowy = "testowy";
+    final String testNullCurrent = null;
+
     @Test
     public void firstTest(){
-        String testCurrent = "testowy";
-        String testNullCurrent = null;
+
+
         assertTrue(true);
         assertTrue(true, "Hello world!");
         assertTrue(5 == 2+3, "Hello world!");
         assertFalse(false, "Hello world!");
-        assertFalse(testCurrent.matches("^s"));
-        assertTrue(testCurrent.equals("testowy"));
-        assertEquals("testowy", testCurrent);
-        assertNotNull(testCurrent);
+        assertFalse(stringTestowy.matches("^s"));
+        assertTrue(stringTestowy.equals("testowy"));
+        assertEquals("testowy", stringTestowy);
+        assertNotNull(stringTestowy);
         assertNull(testNullCurrent, "Mial byc null.");
-        assertSame("testowy", testCurrent);
+        assertSame("testowy", stringTestowy);
 
+        System.out.println(0.2 * 0.2 );
+        assertTrue(new BigDecimal("0.2").multiply(new BigDecimal("0.2")).doubleValue() == 0.04);
+//        assertTrue(0.2 * 0.2 == 0.04);
+
+    }
+
+    @Test
+    public void secondTest(){
+        assertThat(stringTestowy).isEqualTo("testowy");
+        assertThat(testNullCurrent).isNull();
     }
 }
