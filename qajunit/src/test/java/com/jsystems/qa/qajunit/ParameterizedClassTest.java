dadc63b427149376db87429fa22d97fcd4044099
Marcin Kiklica <kiklica.m@gmail.com>
2019-06-10 15:45:11 +0200

    Add second param test

--- qajunit/src/test/java/com/jsystems/qa/qajunit/ParameterizedClassTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/ParameterizedClassTest.java
@@ -10,9 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ParameterizedClassTest {
 
     @DisplayName("First parameterized test")
-    @ParameterizedTest
+    @ParameterizedTest(name = "Run test with value \"{0}\"")
     @ValueSource(ints = {5,15,25})
     public void paramFirstTest(int number){
         assertTrue(number % 5 == 0);
+    }
+
+    @DisplayName("First parameterized test")
+    @ParameterizedTest(name = "Run test with value \"{0}\"")
+    @ValueSource(strings = {"Hello","Hello Junit","Hello World"})
+    public void paramSecondTest(String value){
+        assertTrue(value.contains("Hello"));
     }
 }
