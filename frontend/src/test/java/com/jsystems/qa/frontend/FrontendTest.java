ab99e5c335309ec319c5f29978b3ed13b8b54c75
Marcin Kiklica <kiklica.m@gmail.com>
2019-06-11 15:26:27 +0200

    First test works for firefox. Checking text, is displayed.

--- frontend/src/test/java/com/jsystems/qa/frontend/FrontendTest.java
+++ frontend/src/test/java/com/jsystems/qa/frontend/FrontendTest.java
@@ -2,12 +2,15 @@ package com.jsystems.qa.frontend;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class FrontendTest {
@@ -38,6 +41,14 @@ public class FrontendTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        WebElement login = driver.findElement(By.cssSelector(".x-nav-item--logged-in>.x-link"));
+        WebElement buildTitle = driver.findElement(By.cssSelector(".lpc-headline-title>span:nth-child(1)"));
+        assertTrue(login.isDisplayed());
+        assertTrue(login.getText().equals("Log In"));
+        assertTrue(buildTitle.isDisplayed());
+        assertTrue(buildTitle.getText().equals("Build a website,"));
+        login.click();
+
         driver.quit();
     }
 
