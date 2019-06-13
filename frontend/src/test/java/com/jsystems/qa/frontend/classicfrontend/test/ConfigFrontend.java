c288b96a17b3a021ef909460555dceabd94e1405
Marcin Kiklica <kiklica.m@gmail.com>
2019-06-13 14:11:09 +0200

    Wedriver options.

--- frontend/src/test/java/com/jsystems/qa/frontend/classicfrontend/test/ConfigFrontend.java
+++ frontend/src/test/java/com/jsystems/qa/frontend/classicfrontend/test/ConfigFrontend.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,7 +24,9 @@ public class ConfigFrontend {
 //        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 //        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", ClassLoader.getSystemClassLoader().getResource("drivers/chromedriver.exe").getFile());
+
         System.setProperty("webdriver.gecko.driver", ClassLoader.getSystemClassLoader().getResource("drivers/geckodriver.exe").getFile());
+
     }
 
     @BeforeEach
@@ -35,10 +39,16 @@ public class ConfigFrontend {
     }
 
     private void setBrowserConf(String browser) {
+
         System.out.println("Browser selected: " + browser);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        FirefoxOptions ffOptions = new FirefoxOptions();
+        ffOptions.addArguments("--no-sandbox");
+
         switch (browser) {
-            case "chrome": driver = new ChromeDriver(); System.out.println("Chrome selected."); break;
-            case "firefox": driver = new FirefoxDriver(); System.out.println("Firefox selected."); break;
+            case "chrome": driver = new ChromeDriver(options); System.out.println("Chrome selected."); break;
+            case "firefox": driver = new FirefoxDriver(ffOptions); System.out.println("Firefox selected."); break;
         }
     }
 
